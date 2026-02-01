1b544d06e3d00cbeeb1e83c708b2418c77ade54a
linalinm <maaialin@gmail.com>
2026-02-01 05:37:39 +0200

    command syncing bugfix

--- core/CoreCog.py
+++ core/CoreCog.py
@@ -16,8 +16,6 @@ class CoreCog(commands.Cog):
     async def on_ready(self):
         guild = self.bot.get_guild(int(self._config["Bot"]["server"]))
         self.bot.tree.clear_commands(guild=guild)
-        for command in self.bot.tree.get_commands():
-            if isinstance(command, app_commands.Group):
-                self.bot.tree.add_command(command, guild=guild)
+        self.bot.tree.copy_global_to(guild=guild)
         await self.bot.tree.sync(guild=guild)
         self._logger.log(f"{self.bot.user.name} is now online.")
